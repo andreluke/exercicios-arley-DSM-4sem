3b688800bc0898b8667609e7ce9f692234f2f71d
andreluke <deco99885@gmail.com>
2024-08-26 20:57:29 -0300

    Exercicio 11

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import Um from "./src/Pages/exercicios/ex1";
 import Formulario from "./src/Pages/exercicios/ex2";
+import FATEC from "./src/Pages/exercicios/ex3";
 import Home from "./src/Pages/Home";
 
 const App: React.FC = () => {
   return (
     // <Home />
     // <Um />
-    <Formulario/>
+    // <Formulario/>
+    <FATEC />
   );
 };
 
